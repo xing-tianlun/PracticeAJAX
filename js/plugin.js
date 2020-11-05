b77ba4f17eb43570a7dcfbea4744845e7ec60eb2
xing-tianlun <zhongxunhuanzi@qq.com>
2020-11-05 18:39:08 +0800

    shoppingCart

--- js/plugin.js
+++ js/plugin.js
@@ -4,6 +4,7 @@
     config: {
       ARROW_A: '<span name="minus">-</span><input type="text" value="1" name="count" /><span name="add">+</span><span name="desc">件(库存<label name="num">%num%</label>件)</span></span>',
       ARROW_B: '<ul><li><input type="text" value="1" name="count" /></li><li><span name="add">︿</span><span name="minus">﹀</span></li><li><span name="desc">件(库存<label name="num">%num%</label>件)</span></li></ul>',
+      ARROW_C: '<span name="minus">-</span><input type="text" value="1" name="count" /><span name="add">+</span><span style="display:none;" name="desc">件(库存<label name="num">%num%</label>件)</span></span>',
       PHONE_COLOR: '<span code="%id%" store="%store%">%name%</span>',
       STORE: '<span name="store">%store%</span>'
     },
@@ -12,14 +13,32 @@
     */
     switchTab(target, list, css) {
       [... new Set(list)].forEach(item => {
-        if(item !== target) {
+        if (item !== target) {
           item.className = ''
         }
       })
-      target.className ? target.className='' : target.className = css
+      target.className ? target.className = '' : target.className = css
     },
     operateNumByCount() {
 
+    },
+    checkBoxStatus() {
+      document.body.onmousedown = function (e) {
+        let target = e.target
+        e.stopPropagation();
+        if (target.nodeName == 'I') {
+          if (target.className == 'checked') {
+            target.className = 'unchecked'
+            target.parentNode.parentNode.className = ''
+            return
+          }
+          if (target.className == 'unchecked') {
+            target.className = 'checked'
+            target.parentNode.parentNode.className = 'sc-on'
+            return
+          }
+        }
+      }
     }
   });
 })();
--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   function go() {
     if (!xtl.getCookie('username')) {
       location.href = '../01_index.html'
@@ -8,8 +8,46 @@
   function setUserName() {
     xtl.$qS('span[name=login]').innerText = xtl.getCookie('username')
   }
+  function operateFn() {
+    /*
+    * @描述 操作checkbox
+    */
+    xtl.$qS('i[name=ck]').onclick = function (e) {
+      let chks = xtl.$qSA('i[name=chk]')
+      for (let i = 0; i < chks.length; i++) {
+        if (this.className === 'checked') {
+          chks[i].className = 'unchecked'
+          chks[i].parentNode.parentNode.className = ''
+        }
+        if (this.className === 'unchecked') {
+          chks[i].className = 'checked'
+          chks[i].parentNode.parentNode.className = 'sc-on'
+        }
+      }
+    }
+    xtl.$qS('ul.sc').onmouseup = function (e) {
+      let target = e.target
+      let ck = xtl.$qS('i[name=ck]')
+      let chks = xtl.$qSA('i[name=chk]')
+      let flag = true;
+      if (target.nodeName == 'I') {
+        if (target.getAttribute('name') === 'chk') {
+          for (let i = 0; i < chks.length; i++) {
+            if (chks[i].className == 'unchecked') {
+              console.log(1);
+              flag = false
+              break
+            }
+          }
+          ck.className = flag ? 'checked' : 'unchecked';
+        }
+      }
+    }
+  }
   function init() {
     go()
+    xtl.plugin.checkBoxStatus()
+    operateFn()
   }
   init()
 })();
